c09e74bc9e31c3bc2792e717d0279a2a4426efa5
nari2025 <eina-ichinari@kiseki.systems>
2025-09-04 17:26:00 +0900

    add: DeptConstants.java マジックナンバーを定数化
    狙い:　保守性の向上
    部署ナンバーを定数化し、DeptConstantsクラスへ移行した。

---
+++ refactoring_app_name/src/jp/co/sss/crud/constants/DeptConstants.java
@@ -0,0 +1,10 @@
+package jp.co.sss.crud.constants;
+
+public class DeptConstants {
+
+	/** 部署の定数化 */
+	public static final int ACCOUNTING_DEPT = 1;
+	public static final int SALES_DEPT = 2;
+	public static final int GENERAL_AFFAIRS_DEPT = 3;
+
+}
