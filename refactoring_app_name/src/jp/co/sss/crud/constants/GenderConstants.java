ea7d55d43117c1c240b3209618f00fcf454bbcc3
nari2025 <eina-ichinari@kiseki.systems>
2025-09-04 17:25:01 +0900

    add: GenderConstants.java マジックナンバーを定数化
    狙い:　保守性の向上
    部署ナンバーを定数化し、GenderConstantsクラスへ移行した。

---
+++ refactoring_app_name/src/jp/co/sss/crud/constants/GenderConstants.java
@@ -0,0 +1,10 @@
+package jp.co.sss.crud.constants;
+
+public class GenderConstants {
+
+	/** 性別の定数化 */
+	public static final int NO_ANSWER = 0;
+	public static final int MAN = 1;
+	public static final int FEMALE = 2;
+	public static final int OTHER = 9;
+}
