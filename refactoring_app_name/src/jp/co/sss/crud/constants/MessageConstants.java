740fccda00737117b1f5c008838f081bf33a3a6f
nari2025 <eina-ichinari@kiseki.systems>
2025-09-04 17:29:07 +0900

    fix: MessageConstants.java メッセージを定数化
    狙い:　保守性の向上
    メッセージを定数化し、MessageConstantsクラスに追加した

--- refactoring_app_name/src/jp/co/sss/crud/constants/MessageConstants.java
+++ refactoring_app_name/src/jp/co/sss/crud/constants/MessageConstants.java
@@ -3,4 +3,14 @@ package jp.co.sss.crud.constants;
 public class MessageConstants {
 	/** 検索時に該当者がいない場合のメッセージ */
 	public static final String MSG_NO_MATCH_FOUND = "該当者はいませんでした";
+	/** 更新する社員を質問する時のメッセージ */
+	public static final String MSG_UPDATE_QUESTION = "更新する社員の社員IDを入力してください：";
+	/** 削除する社員を質問する時のメッセージ */
+	public static final String MSG_DELETE_QUESTION = "削除する社員の社員IDを入力してください：";
+	/** 更新時のメッセージ */
+	public static final String MSG_UPDATE_COMPLETE = "社員情報を更新しました";
+	/** 削除時のメッセージ */
+	public static final String MSG_DELETE_COMPLETE = "社員情報を削除しました";
+	/** 終了時のメッセージ */
+	public static final String MSG_FINISH = "システムを終了します。";
 }
