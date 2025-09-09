33c0600580d4e0cfc5af09132052a51cf075eb17
nari2025 <eina-ichinari@kiseki.systems>
2025-09-09 11:30:52 +0900

    fix: SystemErrorException.java
    狙い:保守性の向上
    リファクタリング演習のパッケージ構成に沿って、exceptionクラスを作成

---
+++ refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
@@ -0,0 +1,11 @@
+package jp.co.sss.crud.exception;
+
+import java.io.IOException;
+
+public class SystemErrorException extends Exception {
+
+	public SystemErrorException(String msgSystemError, IOException e) {
+		// TODO 自動生成されたコンストラクター・スタブ
+	}
+
+}
