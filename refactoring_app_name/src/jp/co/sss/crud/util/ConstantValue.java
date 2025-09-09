ff7f13c21984ba25df04bb765262b1d2ccc05f58
nari2025 <eina-ichinari@kiseki.systems>
2025-09-09 11:16:30 +0900

    fix: ConstantValue.java
    部署に対応する数字が異なっていたので修正

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
@@ -1,17 +1,6 @@
 package jp.co.sss.crud.util;
 
 public class ConstantValue {
-	/** 性別の定数化 */
-	public static final int NO_ANSWER = 0;
-	public static final int MALE = 1;
-	public static final int FEMALE = 2;
-	public static final int OTHER = 9;
-
-	/** 部署の定数化 */
-	public static final int ACCOUNTING_DEPT = 1;
-	public static final int SALES_DEPT = 2;
-	public static final int GENERAL_AFFAIRS_DEPT = 3;
-
 	/** 入力された数字に対応した処理を変数として定義 */
 	public static final int FIND_ALL = 1;
 	public static final int FIND_NAME = 2;
@@ -20,4 +9,16 @@ public class ConstantValue {
 	public static final int UPDATE = 5;
 	public static final int DELETE = 6;
 	public static final int FINISH = 7;
+
+	/** 性別の定数化 */
+	public static final int NO_ANSWER = 0;
+	public static final int MALE = 1;
+	public static final int FEMALE = 2;
+	public static final int OTHER = 9;
+
+	/** 部署の定数化 */
+	public static final int SALES_DEPT = 1;
+	public static final int ACCOUNTING_DEPT = 2;
+	public static final int GENERAL_AFFAIRS_DEPT = 3;
+
 }
