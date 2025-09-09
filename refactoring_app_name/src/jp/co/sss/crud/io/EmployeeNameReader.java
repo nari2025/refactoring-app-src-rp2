b6aafbb0b8118173fda816eb2635973425ab9aca
nari2025 <eina-ichinari@kiseki.systems>
2025-09-09 17:54:11 +0900

    fix: EmployeeNameReader.java
    狙い:保守性の向上、信頼性の向上
    
    バリデーションの追加

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeNameReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeNameReader.java
@@ -1,5 +1,20 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeNameReader {
+public class EmployeeNameReader implements IConsoleReader {
+
+	@Override
+	public String getErrorMsg() {
+		return "1文字以上30文字以下の文字列を入力してください";
+	}
+
+	@Override
+	public boolean isValid(String inputString) {
+		return inputString.length() >= 1 && 30 >= inputString.length();
+	}
+
+	@Override
+	public boolean isParseInt() {
+		return false;
+	}
 
 }
