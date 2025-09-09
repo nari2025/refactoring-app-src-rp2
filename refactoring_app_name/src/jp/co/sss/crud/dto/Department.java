2842d3d71b85cd58b4a79c53d7f345e79173a438
nari2025 <eina-ichinari@kiseki.systems>
2025-09-09 11:20:39 +0900

    fix: employee.java
    狙い:保守性の向上
    DTOを導入し、toStringメソッドを追加

--- refactoring_app_name/src/jp/co/sss/crud/dto/Department.java
+++ refactoring_app_name/src/jp/co/sss/crud/dto/Department.java
@@ -1,7 +1,29 @@
 package jp.co.sss.crud.dto;
 
 public class Department {
+
 	private String deptName;
+	private int deptId;
+
+	private Employee employee;
+
+	@Override
+	public String toString() {
+
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(employee.getEmpId());
+		sb.append("\t");
+		sb.append(employee.getEmpName());
+		sb.append("\t");
+		sb.append(employee.getGenderText(employee.getGender()));
+		sb.append("\t");
+		sb.append(employee.getBirthday());
+		sb.append("\t");
+		sb.append(deptName);
+		sb.append("\t");
+		return sb.toString();
+	}
 
 	public String getDeptName() {
 		return deptName;
@@ -9,5 +31,21 @@ public class Department {
 
 	public void setDeptName(String deptName) {
 		this.deptName = deptName;
+	}
+
+	public int getDeptId() {
+		return deptId;
+	}
+
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+
+	public Employee getEemployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
 	}
 }
