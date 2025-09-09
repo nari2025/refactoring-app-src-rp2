44ff6a73082c6a269b59a7f72952703e56b1d69d
nari2025 <eina-ichinari@kiseki.systems>
2025-09-09 17:53:56 +0900

    fix: MenuNoReader.java
    狙い:保守性の向上、信頼性の向上
    
    バリデーションの追加

--- refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
@@ -1,5 +1,20 @@
 package jp.co.sss.crud.io;
 
-public class MenuNoReader {
+public class MenuNoReader implements IConsoleReader {
+
+	@Override
+	public String getErrorMsg() {
+		return "1以上7以下の整数を入力してください";
+	}
+
+	@Override
+	public boolean isValid(String inputString) {
+		return inputString.matches("^[1-7１-７]$");
+	}
+
+	@Override
+	public boolean isParseInt() {
+		return true;
+	}
 
 }
