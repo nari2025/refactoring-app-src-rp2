72bd8f78876db0cb85e6dea44574e971be02ce1e
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 13:48:46 +0900

    fix: IllegalInputException.java, SystemErrorException.java
    どんな例外でもこのコンストラクタで処理できるようにした。

--- refactoring_app_name/src/jp/co/sss/crud/exception/IllegalInputException.java
+++ refactoring_app_name/src/jp/co/sss/crud/exception/IllegalInputException.java
@@ -3,7 +3,7 @@ package jp.co.sss.crud.exception;
 public class IllegalInputException extends Exception {
 
 	public IllegalInputException(String errorMsg) {
-		// TODO 自動生成されたコンストラクター・スタブ
+		super(errorMsg);
 	}
 
 }
--- refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
+++ refactoring_app_name/src/jp/co/sss/crud/exception/SystemErrorException.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class SystemErrorException extends Exception {
 
 	public SystemErrorException(String msgSystemError, IOException e) {
-		// TODO 自動生成されたコンストラクター・スタブ
+		super(msgSystemError, e);
 	}
 
 }
