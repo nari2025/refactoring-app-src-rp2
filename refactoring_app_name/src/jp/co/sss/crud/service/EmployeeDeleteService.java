e078a32bfd62fe0279f63a6af4e48e8a0455d36f
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:15:40 +0900

    fix: EmployeeDeleteService.java
    狙い:保守性の向上、拡張性の向上
    EmployeeDAO.javaクラスに記述すべきでない処理をサービスクラスに記述しなおした。

--- refactoring_app_name/src/jp/co/sss/crud/service/EmployeeDeleteService.java
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeDeleteService.java
@@ -5,6 +5,8 @@ import java.sql.SQLException;
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
+import jp.co.sss.crud.io.ConsoleWriter;
+import jp.co.sss.crud.io.EmployeeEmpIdReader;
 
 public class EmployeeDeleteService implements IEmployeeService {
 	// DAOのインスタンスを保持
@@ -18,10 +20,15 @@ public class EmployeeDeleteService implements IEmployeeService {
 	 */
 	@Override
 	public void execute() throws SystemErrorException, IllegalInputException {
+		ConsoleWriter.showDeleteQuestion();
+		//削除する社員のIDを入力
+		EmployeeEmpIdReader employeeEmpIdReader = new EmployeeEmpIdReader();
+		int empId = (Integer) employeeEmpIdReader.input();
 		try {
-			employeeDAO.delete();
+			employeeDAO.delete(empId);
 		} catch (ClassNotFoundException | SQLException | SystemErrorException | IllegalInputException e) {
 			e.printStackTrace();
 		}
+
 	}
 }
