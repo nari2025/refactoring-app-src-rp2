12e10b963907cdedbf331e3f4e35db88464bd2fc
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:16:02 +0900

    fix: EmployeeFindByDeptIdService.java
    狙い:保守性の向上、拡張性の向上
    EmployeeDAO.javaクラスに記述すべきでない処理をサービスクラスに記述しなおした。

--- refactoring_app_name/src/jp/co/sss/crud/service/EmployeeFindByDeptIdService.java
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeFindByDeptIdService.java
@@ -5,10 +5,11 @@ import java.sql.SQLException;
 import java.util.List;
 
 import jp.co.sss.crud.db.EmployeeDAO;
-import jp.co.sss.crud.dto.Department;
+import jp.co.sss.crud.dto.Employee;
 import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
-import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.io.ConsoleWriter;
+import jp.co.sss.crud.io.EmployeeDeptIdReader;
 
 public class EmployeeFindByDeptIdService implements IEmployeeService {
 	// DAOのインスタンスを保持
@@ -25,18 +26,17 @@ public class EmployeeFindByDeptIdService implements IEmployeeService {
 	 */
 	@Override
 	public void execute() throws SystemErrorException, IllegalInputException {
+		ConsoleWriter.showDeptIdQuestion();
+		//検索する部署ID
+		EmployeeDeptIdReader employeeDeptIdReader = new EmployeeDeptIdReader();
+		int deptId = (Integer) employeeDeptIdReader.input();
+
 		try {
 			// DAOのfindByDeptId()メソッドを呼び出してリストを取得
-			List<Department> department = employeeDAO.findByDeptId();
-			// レコードを出力
-			System.out.println(ConstantMsg.MSG_LIST_CALAM);
-			//取得したリストをループで表示
-			for (Department emp : department) {
-				System.out.println(emp);
-			}
-			System.out.println("");
+			List<Employee> searchedEmployees = employeeDAO.findByDeptId(deptId);
+			//結果をコンソール出力
+			ConsoleWriter.showEmployees(searchedEmployees);
 		} catch (ClassNotFoundException | SQLException | IOException e) {
-			// エラーハンドリング（例：エラーメッセージの表示）
 			e.printStackTrace();
 		}
 	}
