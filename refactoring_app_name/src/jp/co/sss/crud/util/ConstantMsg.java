9e0459ad02590e742e09d7c661910fdb61ed3d03
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:11:26 +0900

    fix: ConstantMsg.java
    狙い:保守性の向上
    定数化したメッセージの追加

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantMsg.java
@@ -4,8 +4,10 @@ public class ConstantMsg {
 
 	/** 全件表示の際のテーブルのカラム名 */
 	public static final String MSG_LIST_CALAM = "社員ID\t社員名\t性別\t生年月日\t部署名";
+	/** 全件表示の際のテーブルのカラム名 */
+	public static final String MSG_FIND_BY_DEPTNO = "部署ID(1：営業部、2：経理部、3：総務部)を入力してください：";
 	/** 検索時に該当者がいない場合のメッセージ */
-	public static final String MSG_NO_MATCH_FOUND = "該当者はいませんでした";
+	public static final String MSG_NO_MATCH_FOUND = "該当する社員は存在しません。";
 	/** 更新する社員を質問する時のメッセージ */
 	public static final String MSG_UPDATE_QUESTION = "更新する社員の社員IDを入力してください：";
 	/** 削除する社員を質問する時のメッセージ */
@@ -19,5 +21,6 @@ public class ConstantMsg {
 	/** 終了時のメッセージ */
 	public static final String MSG_FINISH = "システムを終了します。";
 	public static final String MSG_SYSTEM_ERROR = "エラーが発生しました。";
+	public static final String MSG_NO_TARGET_FOUND = "対象者がいませんでした";
 
 }
