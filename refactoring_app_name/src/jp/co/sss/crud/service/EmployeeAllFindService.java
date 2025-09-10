a7336136a37c0ca94d14c2ec9a65c524f151219c
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:08:35 +0900

    fix: EmployeeAllFindService.java
    狙い:保守性の向上、拡張性の向上
    ConsoleWriter.javaにコンソール出力をまとめた。

--- refactoring_app_name/src/jp/co/sss/crud/service/EmployeeAllFindService.java
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeAllFindService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.dto.Employee;
-import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.io.ConsoleWriter;
 
 public class EmployeeAllFindService implements IEmployeeService {
 	// DAOのインスタンスを保持
@@ -17,20 +17,11 @@ public class EmployeeAllFindService implements IEmployeeService {
 	@Override
 	public void execute() {
 		try {
-			// レコードを出力
-			System.out.println(ConstantMsg.MSG_LIST_CALAM);
-
 			// DAOのfindAll()メソッドを呼び出してリストを取得
 			List<Employee> allEmployees = employeeDAO.findAll();
-
-			// 取得したリストをループ処理で表示
-			for (Employee emp : allEmployees) {
-				System.out.println(emp);
-			}
-			System.out.println("");
-
+			//リストをコンソール出力
+			ConsoleWriter.showEmployees(allEmployees);
 		} catch (ClassNotFoundException | SQLException e) {
-			// エラーハンドリング（例：エラーメッセージの表示）
 			e.printStackTrace();
 		}
 	}
