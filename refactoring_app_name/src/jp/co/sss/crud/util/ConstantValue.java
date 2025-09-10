a2b7716c8fc2288b309fe2ea83775f904c2e1bf3
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 13:40:44 +0900

    fix: ConstantValue.java
    狙い:保守性の向上
    マジックナンバーを定数化

--- refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
+++ refactoring_app_name/src/jp/co/sss/crud/util/ConstantValue.java
@@ -1,13 +1,7 @@
 package jp.co.sss.crud.util;
 
 public class ConstantValue {
-	// 定数をDTOクラス内に定義するか、別の定数クラスにまとめる
-	public static final String STRING_MALE = "男性";
-	public static final String STRING_FEMALE = "女性";
-	public static final String STRING_OTHER = "その他";
-	public static final String STRING_NO_ANSWER = "回答なし";
-
-	/** 入力された数字に対応した処理を変数として定義 */
+	/** メニュー番号の定数化 */
 	public static final int FIND_ALL = 1;
 	public static final int FIND_NAME = 2;
 	public static final int FIND_ID = 3;
@@ -16,15 +10,29 @@ public class ConstantValue {
 	public static final int DELETE = 6;
 	public static final int FINISH = 7;
 
+	/** プレースホルダーの定数化 */
+	public static final int NAME_PARAM_INDEX = 1;
+	public static final int GENDER_PARAM_INDEX = 2;
+	public static final int BIRTHDAY_PARAM_INDEX = 3;
+	public static final int DEPTID_PARAM_INDEX = 4;
+	public static final int EMPID_PARAM_INDEX = 5;
+	public static final int DELETE_EMP_ID_PARAM_INDEX = 1;
+	public static final int FINDBY_DEPTID_PARAM_INDEX = 1;
+
 	/** 性別の定数化 */
 	public static final int NO_ANSWER = 0;
 	public static final int MALE = 1;
 	public static final int FEMALE = 2;
 	public static final int OTHER = 9;
 
+	// 性別（日本語）の定数化
+	public static final String STRING_MALE = "男性";
+	public static final String STRING_FEMALE = "女性";
+	public static final String STRING_OTHER = "その他";
+	public static final String STRING_NO_ANSWER = "回答なし";
+
 	/** 部署の定数化 */
 	public static final int SALES_DEPT = 1;
 	public static final int ACCOUNTING_DEPT = 2;
 	public static final int GENERAL_AFFAIRS_DEPT = 3;
-
 }
