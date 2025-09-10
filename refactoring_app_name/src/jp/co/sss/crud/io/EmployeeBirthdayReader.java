57e2cbffdd297b0183917d1c7ab671f1e648bc15
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:10:45 +0900

    fix: EmployeeBirthdayReader.java
    狙い:保守性の向上、拡張性の向上
    入力チェックを導入した。

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeBirthdayReader.java
@@ -1,5 +1,34 @@
 package jp.co.sss.crud.io;
 
-public class EmployeeBirthdayReader {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
+public class EmployeeBirthdayReader implements IConsoleReader {
+
+	@Override
+	public String getErrorMsg() {
+		return "正しい形式(西暦年/月/日)で日付を入力してください";
+	}
+
+	@Override
+	public boolean isValid(String inputString) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		// setLenient(false)で厳密な日付チェックを行う（例: 2023/02/30は無効）
+		sdf.setLenient(false);
+
+		try {
+			// parseメソッドで文字列をDate型に変換してみる
+			sdf.parse(inputString);
+			// 変換に成功すれば有効
+			return true;
+		} catch (ParseException e) {
+			// 変換に失敗すれば無効
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isParseInt() {
+		return false;
+	}
 }
