1d557e57797fa571147f3560a7f4f2614571f96e
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 13:45:39 +0900

    fix: EmployeeUpdateService.java, EmployeeRegisterService.java
    狙い:拡張性の向上、保守性の向上
    処理を共通化した。

--- refactoring_app_name/src/jp/co/sss/crud/service/EmployeeRegisterService.java
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeRegisterService.java
@@ -6,11 +6,6 @@ import java.text.ParseException;
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
-import jp.co.sss.crud.io.ConsoleWriter;
-import jp.co.sss.crud.io.EmployeeBirthdayReader;
-import jp.co.sss.crud.io.EmployeeDeptIdReader;
-import jp.co.sss.crud.io.EmployeeGenderReader;
-import jp.co.sss.crud.io.EmployeeNameReader;
 
 public class EmployeeRegisterService implements IEmployeeService {
 	// DAOのインスタンスを保持
@@ -26,25 +21,8 @@ public class EmployeeRegisterService implements IEmployeeService {
 	@Override
 	public void execute() throws SystemErrorException, IllegalInputException {
 
-		// 名前を入力
-		EmployeeNameReader employeeNameReader = new EmployeeNameReader();
-		ConsoleWriter.showEmpNameQuestion();
-		String empName = (String) employeeNameReader.input();
-		// 性別を入力
-		EmployeeGenderReader employeeGenderReader = new EmployeeGenderReader();
-		ConsoleWriter.showGenderQuestion();
-		int gender = (Integer) employeeGenderReader.input();
-		// 誕生日を入力
-		EmployeeBirthdayReader employeeBirthdayReader = new EmployeeBirthdayReader();
-		ConsoleWriter.showBirthDatQuestion();
-		String birthday = (String) employeeBirthdayReader.input();
-		// 部署IDを入力
-		EmployeeDeptIdReader employeeDeptIdReader = new EmployeeDeptIdReader();
-		ConsoleWriter.showDeptQuestion();
-		int deptId = (Integer) employeeDeptIdReader.input();
-
 		try {
-			employeeDAO.insert(empName, gender, birthday, deptId);
+			employeeDAO.insert();
 		} catch (ClassNotFoundException | SQLException | ParseException e) {
 			e.printStackTrace();
 		}
