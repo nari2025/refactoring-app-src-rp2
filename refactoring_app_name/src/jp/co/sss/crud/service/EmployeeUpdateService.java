4e209dd87a8bb22188b698ce00fffcae3fe394a8
nari2025 <eina-ichinari@kiseki.systems>
2025-09-10 11:16:52 +0900

    fix: EmployeeUpdateService.java, EmployeeRegisterService.java
    狙い:保守性の向上、拡張性の向上
    EmployeeDAO.javaクラスに記述すべきでない処理をサービスクラスに記述しなおした。

--- refactoring_app_name/src/jp/co/sss/crud/service/EmployeeUpdateService.java
+++ refactoring_app_name/src/jp/co/sss/crud/service/EmployeeUpdateService.java
@@ -7,7 +7,12 @@ import java.text.ParseException;
 import jp.co.sss.crud.db.EmployeeDAO;
 import jp.co.sss.crud.exception.IllegalInputException;
 import jp.co.sss.crud.exception.SystemErrorException;
-import jp.co.sss.crud.util.ConstantMsg;
+import jp.co.sss.crud.io.ConsoleWriter;
+import jp.co.sss.crud.io.EmployeeBirthdayReader;
+import jp.co.sss.crud.io.EmployeeDeptIdReader;
+import jp.co.sss.crud.io.EmployeeEmpIdReader;
+import jp.co.sss.crud.io.EmployeeGenderReader;
+import jp.co.sss.crud.io.EmployeeNameReader;
 
 public class EmployeeUpdateService implements IEmployeeService {
 	// DAOのインスタンスを保持
@@ -22,10 +27,32 @@ public class EmployeeUpdateService implements IEmployeeService {
 	 */
 	@Override
 	public void execute() throws SystemErrorException, IllegalInputException {
-		System.out.print(ConstantMsg.MSG_UPDATE_QUESTION);
+		ConsoleWriter.showUpdateQuestion();
+		//更新する社員のID
+		EmployeeEmpIdReader employeeEmpIdReader = new EmployeeEmpIdReader();
+		int empId = (Integer) employeeEmpIdReader.input();
+
+		// 名前を入力
+		EmployeeNameReader employeeNameReader = new EmployeeNameReader();
+		ConsoleWriter.showEmpNameQuestion();
+		String empName = (String) employeeNameReader.input();
+		// 性別を入力
+		EmployeeGenderReader employeeGenderReader = new EmployeeGenderReader();
+		ConsoleWriter.showGenderQuestion();
+		int gender = (Integer) employeeGenderReader.input();
+		// 誕生日を入力
+		EmployeeBirthdayReader employeeBirthdayReader = new EmployeeBirthdayReader();
+		ConsoleWriter.showBirthDatQuestion();
+		String birthday = (String) employeeBirthdayReader.input();
+		// 部署IDを入力
+		EmployeeDeptIdReader employeeDeptIdReader = new EmployeeDeptIdReader();
+		ConsoleWriter.showDeptQuestion();
+		int deptId = (Integer) employeeDeptIdReader.input();
 
 		try {
-			employeeDAO.update();
+			int updatedRows = employeeDAO.update(empId, empName, gender, birthday, deptId);
+			ConsoleWriter.showUpdateComp(updatedRows);
+
 		} catch (ClassNotFoundException | SQLException | IOException | ParseException e) {
 			e.printStackTrace();
 		}
