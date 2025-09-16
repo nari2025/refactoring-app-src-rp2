97bfc59de1930057b90ae662f32a42df4c55196b
nari2025 <eina-ichinari@kiseki.systems>
2025-09-16 15:22:32 +0900

    fix: EmployeeEmpIdReader.java
    狙い:可読性の向上
    メソッドに対するコメントを追加した。

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeEmpIdReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeEmpIdReader.java
@@ -2,18 +2,29 @@ package jp.co.sss.crud.io;
 
 import jp.co.sss.crud.util.ConstantMsg;
 
-public class  implements IConsoleReader {
-
+public class EmployeeEmpIdReader implements IConsoleReader {
+	/**
+	 * エラーメッセージの表示
+	 * @return エラーメッセージ
+	 */
 	@Override
 	public String getErrorMsg() {
 		return ConstantMsg.MSG_EMPID_ERROR;
 	}
 
+	/**
+	 * バリデーションチェック
+	 * @return バリデーションの結果
+	 */
 	@Override
 	public boolean isValid(String inputString) {
 		return inputString.matches("^[1-9１-９]{1}[0-9１-９]{0,3}$");
 	}
 
+	/**
+	 * 数字かどうかの判定
+	 * @return 数字のため、trueを返す
+	 */
 	@Override
 	public boolean isParseInt() {
 		return true;
