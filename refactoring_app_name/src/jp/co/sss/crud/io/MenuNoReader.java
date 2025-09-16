702b3132b4dcbd7e4d34115e88f6a4b657182deb
nari2025 <eina-ichinari@kiseki.systems>
2025-09-16 15:23:50 +0900

    fix: MenuNoReader.java
    狙い:可読性の向上
    メソッドに対するコメントを追加した。

--- refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/MenuNoReader.java
@@ -1,17 +1,30 @@
 package jp.co.sss.crud.io;
 
-public class MenuNoReader implements IConsoleReader {
+import jp.co.sss.crud.util.ConstantMsg;
 
+public class MenuNoReader implements IConsoleReader {
+	/**
+	 * エラーメッセージの表示
+	 * @return エラーメッセージ
+	 */
 	@Override
 	public String getErrorMsg() {
-		return "1以上7以下の整数を入力してください";
+		return ConstantMsg.MSG_MENUNO_ERROR;
 	}
 
+	/**
+	 * バリデーションチェック
+	 * @return バリデーションの結果
+	 */
 	@Override
 	public boolean isValid(String inputString) {
 		return inputString.matches("^[1-7１-７]$");
 	}
 
+	/**
+	 * 数字かどうかの判定
+	 * @return 数字のため、trueを返す
+	 */
 	@Override
 	public boolean isParseInt() {
 		return true;
