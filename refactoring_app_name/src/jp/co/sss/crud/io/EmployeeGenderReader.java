9755b7f8222b6e48355bbb37d0eb43fb97abb51e
nari2025 <eina-ichinari@kiseki.systems>
2025-09-16 15:23:13 +0900

    fix: EmployeeGenderReader.java
    狙い:可読性の向上
    メソッドに対するコメントを追加した。

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeGenderReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeGenderReader.java
@@ -3,17 +3,28 @@ package jp.co.sss.crud.io;
 import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeGenderReader implements IConsoleReader {
-
+	/**
+	 * エラーメッセージの表示
+	 * @return エラーメッセージ
+	 */
 	@Override
 	public String getErrorMsg() {
 		return ConstantMsg.MSG_GENDER_ERROR;
 	}
 
+	/**
+	 * バリデーションチェック
+	 * @return バリデーションの結果
+	 */
 	@Override
 	public boolean isValid(String inputString) {
 		return inputString.matches("^|[0129０１２９]{1}$");
 	}
 
+	/**
+	 * 数字かどうかの判定
+	 * @return 数字のため、trueを返す
+	 */
 	@Override
 	public boolean isParseInt() {
 		return true;
