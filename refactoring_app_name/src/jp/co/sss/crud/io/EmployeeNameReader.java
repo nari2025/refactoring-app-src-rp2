bf35bb7337e0863847070d2e7bb3afa03a565a28
nari2025 <eina-ichinari@kiseki.systems>
2025-09-16 15:23:33 +0900

    fix: EmployeeNameReader.java
    狙い:可読性の向上
    メソッドに対するコメントを追加した。

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeNameReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeNameReader.java
@@ -3,17 +3,28 @@ package jp.co.sss.crud.io;
 import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeNameReader implements IConsoleReader {
-
+	/**
+	 * エラーメッセージの表示
+	 * @return エラーメッセージ
+	 */
 	@Override
 	public String getErrorMsg() {
 		return ConstantMsg.MSG_EMPNAME_ERROR;
 	}
 
+	/**
+	 * バリデーションチェック
+	 * @return バリデーションの結果
+	 */
 	@Override
 	public boolean isValid(String inputString) {
 		return inputString.length() >= 1 && 30 >= inputString.length();
 	}
 
+	/**
+	 * 数字かどうかの判定
+	 * @return 数字ではないためfalseを返す
+	 */
 	@Override
 	public boolean isParseInt() {
 		return false;
