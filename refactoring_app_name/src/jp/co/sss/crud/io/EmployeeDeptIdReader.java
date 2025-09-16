3b643d710551222d078b97a5852605e645d07766
nari2025 <eina-ichinari@kiseki.systems>
2025-09-16 15:21:49 +0900

    fix: EmployeeDeptIdReader.java
    狙い:可読性の向上
    メソッドに対するコメントを追加した。

--- refactoring_app_name/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
+++ refactoring_app_name/src/jp/co/sss/crud/io/EmployeeDeptIdReader.java
@@ -3,17 +3,28 @@ package jp.co.sss.crud.io;
 import jp.co.sss.crud.util.ConstantMsg;
 
 public class EmployeeDeptIdReader implements IConsoleReader {
-
+	/**
+	 * エラーメッセージの表示
+	 * @return エラーメッセージ
+	 */
 	@Override
 	public String getErrorMsg() {
 		return ConstantMsg.MSG_DEPTID_ERROR;
 	}
 
+	/**
+	 * バリデーションチェック
+	 * @return バリデーションの結果
+	 */
 	@Override
 	public boolean isValid(String inputString) {
 		return inputString.matches("^[1-3１-３]{1}$");
 	}
 
+	/**
+	 * 数字かどうかの判定
+	 * @return 数字のため、trueを返す
+	 */
 	@Override
 	public boolean isParseInt() {
 		return true;
